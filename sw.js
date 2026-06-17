a175364e1bfa4d2b8679c73e6030b414e5ab6ba6
CDKe-2022 <104467212+CDKe-2022@users.noreply.github.com>
2026-06-17 17:19:16 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,30 @@
-const CACHE_NAME = 'toolbox-v1.1';
+const CACHE_NAME = 'toolbox-v1';
 const ASSETS = [
   '/',
   '/index.html',
   '/assets/css/common.css',
   '/assets/js/common.js',
+  '/assets/js/components.js',
   '/assets/js/tools-data.js',
-  '/assets/js/home.js',
+  '/assets/js/home.js', // 新增
   '/tools/number-to-chinese.html',
-  '/tools/number-to-chinese.js',
   '/tools/image-to-pdf.html',
-  '/tools/image-to-pdf.js'
+  '/tools/pdf-split-merge.html'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    }).then(() => self.clients.claim())
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
 
